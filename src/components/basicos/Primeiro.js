06498b8813d9eb7d09da49d031a07705d6fad976
Jayme Calixto <jayme.calixto@ambevtech.com.br>
2021-04-27 09:12:15 -0300

    next lesson: https://www.udemy.com/course/react-redux-pt/learn/lecture/19636642#overview

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Primeiro() {
+    const msg = 'aaaaaaa';
+    return (
+        <div>
+            <h2>Primeiro componente</h2>
+            <p>hello { msg }</p>
+        </div>
+    );
+}
