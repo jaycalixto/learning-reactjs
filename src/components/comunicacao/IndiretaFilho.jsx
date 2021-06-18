76099d487000374b720df9dd69a916477d191f90
Jayme Calixto <jayme.calixto@ambevtech.com.br>
2021-06-18 16:32:47 -0300

    next https://www.udemy.com/course/react-redux-pt/learn/lecture/19636832#overview

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function IndiretaFilho ({ quandoClickar }) {
+    const min = 10;
+    const max = 60;
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min;
+    const gerarNerd = () => Math.random() > 0.5;
     return (
         <div>
             <div>Filho</div>
-            <button onClick={() => quandoClickar('João', 44, true)}>Fornecer informações</button>
+            <button onClick={() => quandoClickar('João', gerarIdade(), gerarNerd())}>
+                Fornecer informações
+            </button>
         </div>
         
     );
