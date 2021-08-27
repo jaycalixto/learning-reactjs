fccfff8fa4395cffa706c228e98798df04ea895f
Jayme Calixto <jayme.calixto@ambevtech.com.br>
2021-08-27 16:09:07 -0300

    next https://www.udemy.com/course/react-redux-pt/learn/lecture/23545980#overview

---
+++ navegacao/src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function About(props) {
+  return (<div className="About">
+    <h1>About</h1>
+    <p>nosso sistema foi criado</p>
+  </div>);
+}
+
+export default About;
---
+++ navegacao/src/views/examples/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home(props) {
+  return (<div className="Home"><h1>Home</h1><p>Bem vindo</p></div>);
+}
+
+export default Home;
