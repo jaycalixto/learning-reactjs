6553cafc91056e5215eb332e7328d6dc2920e388
Jayme Calixto <jayme.calixto@ambevtech.com.br>
2021-10-01 16:25:31 -0300

    next https://www.udemy.com/course/react-redux-pt/learn/lecture/23908058#content

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -2,14 +2,26 @@ import React, { useState, useRef, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle';
 
+const merge = (s1, s2) => {
+  return s1+s2;
+};
+
 const UseRef = (props) => {
   const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
+
   const count = useRef(0);
+  const myInput1 = useRef(null);
+  const myInput2 = useRef(null);
 
-  useEffect(() => count.current++, [value1]);
-  /* count.current = count.current + 1; */
-
-  
+  useEffect(() => {
+    count.current++;
+    myInput2.current.focus();
+  }, [value1]);
+  useEffect(() => {
+    count.current++;
+    myInput1.current.focus();
+  }, [value2]);
 
   return (
     <div className="UseRef">
@@ -21,11 +33,15 @@ const UseRef = (props) => {
       <div className="center">
         <div>
           <span className="text">Valor:</span>
-          <span className="text">{value1} &gt;</span>
+          <span className="text">{merge(value1,value2)} &gt;</span>
           <span className="text red">{count.current}</span>
           <span className="text">&lt;</span>
         </div>
-        <input type="text" className="input" value={value1} onChange={(e) => setValue1(e.target.value) } />
+        <input type="text" className="input" ref={myInput1} value={value1} onChange={(e) => setValue1(e.target.value) } />
+      </div>
+      <SectionTitle title="Exercício 02" />
+      <div className="center">
+        <input type="text" className="input" ref={myInput2} value={value2} onChange={(e) => setValue2(e.target.value) } />
       </div>
     </div>
   );
