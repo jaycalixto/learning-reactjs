733a3522088391094c81b8ecb888f79e0ca5e772
Jayme Calixto <jayme.calixto@ambevtech.com.br>
2021-11-26 17:24:09 -0300

    next https://www.udemy.com/course/react-redux-pt/learn/lecture/27736630#notes

---
+++ nextjs/fundamentos/src/pages/estilo.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Estilo.module.css';
+
+export default function Estilo() {
+  return (
+    <div className={styles.roxo}>
+      <h1>Estilo usando CSS Module</h1>
+    </div>
+  );
+}
