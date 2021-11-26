9fc7e18be3c68081979b367ab6022cfcec818b39
Jayme Calixto <jayme.calixto@ambevtech.com.br>
2021-11-26 16:23:13 -0300

    next https://www.udemy.com/course/react-redux-pt/learn/lecture/27736600#notes

--- nextjs/fundamentos/src/pages/index.jsx
+++ nextjs/fundamentos/src/pages/index.jsx
@@ -3,5 +3,10 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return "Início";
+  return (
+    <div>
+      <h1>Sou um título</h1>
+      <h2>sou um subtítulo</h2>
+    </div>
+  );
 }
