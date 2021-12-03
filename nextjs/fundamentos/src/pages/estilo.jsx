8319b668d08a4c9e9df02530fb16d7b5e36c2e79
Jayme Calixto <jayme.calixto@ambevtech.com.br>
2021-12-03 16:20:31 -0300

    next https://www.udemy.com/course/react-redux-pt/learn/lecture/27736644#questions

--- nextjs/fundamentos/src/pages/estilo.jsx
+++ nextjs/fundamentos/src/pages/estilo.jsx
@@ -1,8 +1,10 @@
 import styles from '../styles/Estilo.module.css';
+import Link from 'next/link';
 
 export default function Estilo() {
   return (
     <div className={styles.roxo}>
+      <Link href="/">Voltar</Link>
       <h1>Estilo usando CSS Module</h1>
     </div>
   );
--- nextjs/fundamentos/src/pages/index.jsx
+++ nextjs/fundamentos/src/pages/index.jsx
@@ -1,12 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <h1>Sou um título</h1>
-      <h2>sou um subtítulo</h2>
+      <Link href="/estilo" >
+        Estilo
+      </Link>
+      <Link href="/exemplo" >
+        Exemplo
+      </Link>
     </div>
   );
 }
