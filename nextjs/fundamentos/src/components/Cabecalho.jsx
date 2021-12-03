c7bcc7f4658e7dc826cfe545a08679537fb8090d
Jayme Calixto <jayme.calixto@ambevtech.com.br>
2021-12-03 16:15:04 -0300

    next https://www.udemy.com/course/react-redux-pt/learn/lecture/27736642#questions

--- nextjs/fundamentos/src/components/Cabecalho.jsx
+++ nextjs/fundamentos/src/components/Cabecalho.jsx
@@ -1,7 +1,8 @@
-export default function Cabecalho() {
+export default function Cabecalho({ titulo }) {
   return (
     <header>
-      <h1>Fundamentos de Next.js & React</h1>
+      <h1>{titulo}</h1>
+
     </header>
   );
 }
